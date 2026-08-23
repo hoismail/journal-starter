681cabaa77f075a444eb0f37d2a0350964ecb55b
Houssein Ismail <isma0170@algonquinlive.com>
2026-08-23 14:09:21 -0400

    Imnprt EKS access entry

---
+++ infra/imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.eks.aws_eks_access_entry.github_actions
+  id = "journal-api-eks-cluster:arn:aws:iam::744763865781:role/JournalApiGitHubTerraformRole"
+}
--- infra/modules/observability/main.tf
+++ infra/modules/observability/main.tf
@@ -40,3 +40,8 @@ resource "helm_release" "otel_collector" {
     helm_release.loki,
   ]
 }
+
+import {
+  to = aws_eks_access_entry.github_actions
+  id = "journal-api-eks-cluster:arn:aws:iam::744763865781:role/JournalApiGitHubTerraformRole"
+}
