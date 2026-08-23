d0d55487675941e07508ccd487ddd3d5e1c02cfd
Houssein Ismail <isma0170@algonquinlive.com>
2026-08-23 14:39:34 -0400

    Add namespace ressource

--- infra/modules/observability/main.tf
+++ infra/modules/observability/main.tf
@@ -1,17 +1,22 @@
+resource "kubernetes_namespace_v1" "monitoring" {
+  metadata {
+    name = var.namespace
+  }
+}
+
 resource "helm_release" "kube_prometheus_stack" {
-  name             = "monitoring"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-prometheus-stack"
-  namespace        = var.namespace
-  create_namespace = true
-  skip_crds        = false
+  name       = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = kubernetes_namespace_v1.monitoring.metadata[0].name
+  skip_crds  = false
 }
 
 resource "helm_release" "loki" {
   name       = "loki"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "loki"
-  namespace  = var.namespace
+  namespace  = kubernetes_namespace_v1.monitoring.metadata[0].name
 
   values = [
     var.loki_values
@@ -22,14 +27,14 @@ resource "helm_release" "tempo" {
   name       = "tempo"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "tempo"
-  namespace  = var.namespace
+  namespace  = kubernetes_namespace_v1.monitoring.metadata[0].name
 }
 
 resource "helm_release" "otel_collector" {
   name       = "otel-collector"
   repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart      = "opentelemetry-collector"
-  namespace  = var.namespace
+  namespace  = kubernetes_namespace_v1.monitoring.metadata[0].name
 
   values = [
     var.otel_collector_values
