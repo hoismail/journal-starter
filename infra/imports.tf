c2a72a19f90ee8fac25ecc2ff0f470a2ea5f7e2d
Houssein Ismail <isma0170@algonquinlive.com>
2026-08-26 08:22:11 -0400

    Delete infra/imports.tf

--- infra/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = module.eks.aws_eks_access_entry.github_actions
-  id = "journal-api-eks-cluster:arn:aws:iam::744763865781:role/JournalApiGitHubTerraformRole"
-}
-
-import {
-  to = module.observability.kubernetes_namespace_v1.monitoring
-  id = "monitoring"
-}
